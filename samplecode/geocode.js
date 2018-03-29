48222280b7ddf387b2e244ab24ff50a56df76d4b
samvaran kashyap <samvaran.kashyap@gmail.com>
2018-03-29 09:19:48 -0400

    Added sample codes for distance matrix and geocode

---
+++ samplecode/geocode.js
@@ -0,0 +1,13 @@
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  Promise: Promise
+});
+
+googleMapsClient.geocode({address: '1600 Amphitheatre Parkway, Mountain View, CA'})
+  .asPromise()
+  .then((response) => {
+    console.log(response.json.results);
+  })
+  .catch((err) => {
+    console.log(err);
+});
