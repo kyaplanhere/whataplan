c303952d6866b8415b8beacf835eb8b287d4efd8
samvaran kashyap <samvaran.kashyap@gmail.com>
2018-03-28 21:42:23 -0400

    Added config.json for app configs

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,7 @@
+var config = require("../config.json")
+
+console.log(config);
+
 var appRouter = function (app) {
     app.get("/", function(req, res){
       res.status(200).send("Welcome to our whataplan restful API");
